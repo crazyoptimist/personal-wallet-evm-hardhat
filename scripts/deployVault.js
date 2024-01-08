cf0f75d6167e725eb0ac87a1a73560f3a7e7541b
crazyoptimist <hey@crazyoptimist.net>
2024-01-08 15:12:56 -0600

    feat: finish deploy script

--- scripts/deployVault.js
+++ scripts/deployVault.js
@@ -1,8 +1,13 @@
-import { ethers } from "hardhat";
+const { ethers } = require("hardhat");
 
 async function main() {
-  const vault = await ethers.deployContract("contracts/Vault.sol:Vault");
+  const [owner] = await ethers.getSigners();
+
+  const vault = await ethers.deployContract("contracts/Vault.sol:Vault", owner);
   await vault.waitForDeployment();
+
+  const vaultAddress = await vault.getAddress();
+  console.log(`Deployed the Vault contract at ${vaultAddress}`);
 }
 
 main().catch((error) => {
