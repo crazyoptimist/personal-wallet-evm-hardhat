14f24f91d17a99d66d9ef474a9d117f5e066b7e3
crazyoptimist <hey@crazyoptimist.net>
2024-01-20 23:42:26 -0600

    feat: migrate to use hardhat config variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-verify");
-require("dotenv").config();
 
 const ETH_FORK_BLOCK_NUMBER = 18963379;
 
@@ -10,19 +9,19 @@ const COMPILERS = [
   },
 ];
 
-const OWNER_PRIVATE_KEY = process.env.OWNER_PRIVATE_KEY;
+const OWNER_PRIVATE_KEY = vars.get("OWNER_PRIVATE_KEY");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: process.env.ETH_URL,
+        url: vars.get("ETH_URL"),
         blockNumber: ETH_FORK_BLOCK_NUMBER,
       },
     },
     ethereum: {
-      url: process.env.ETH_URL,
+      url: vars.get("ETH_URL"),
       accounts: [OWNER_PRIVATE_KEY],
     },
   },
@@ -31,7 +30,7 @@ module.exports = {
   },
   // Below 2 attributes are for contract verification
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: vars.get("ETHERSCAN_API_KEY"),
   },
   sourcify: {
     enabled: true,
